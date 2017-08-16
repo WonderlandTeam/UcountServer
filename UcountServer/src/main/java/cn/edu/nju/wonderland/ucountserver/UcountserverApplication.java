a25bb47b3eb0c2a9aa8e7f5bcf251fe52e7630e3
HuangXiao <hx36w35@163.com>
2017-08-16 12:28:06 +0800

    Create Project

---
+++ UcountServer/src/main/java/cn/edu/nju/wonderland/ucountserver/UcountserverApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nju.wonderland.ucountserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcountserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcountserverApplication.class, args);
+	}
+}
