543390ecb83c823d0c21896cad56f45093cf1d3e
HuangXiao <hx36w35@163.com>
2017-08-19 00:31:16 +0800

    远程数据库访问配置

--- UcountServer/src/main/java/cn/edu/nju/wonderland/ucountserver/vo/PostInfoVO.java
+++ UcountServer/src/main/java/cn/edu/nju/wonderland/ucountserver/vo/PostInfoVO.java
@@ -1,5 +1,39 @@
 package cn.edu.nju.wonderland.ucountserver.vo;
 
+import cn.edu.nju.wonderland.ucountserver.entity.Post;
+
+import java.util.List;
+
+/**
+ * 帖子信息VO
+ */
 public class PostInfoVO {
-    // TODO 帖子信息VO
+
+    public String username;             // 用户名
+    public String title;                // 标题
+    public String content;              // 内容
+    public String time;                 // 时间
+    public int supportNum;           // 点赞数
+    public List<PostReplyVO> replies;   // 回复（按赞数、时间排序初始10条）
+
+    public PostInfoVO() {
+    }
+
+    public PostInfoVO(Post entity) {
+        this.username = entity.getUsername();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.time = entity.getTime();
+        this.supportNum = entity.getSupportNum();
+    }
+
+    public PostInfoVO(String username, String title, String content, String time, int supportNum, List<PostReplyVO> replies) {
+        this.username = username;
+        this.title = title;
+        this.content = content;
+        this.time = time;
+        this.supportNum = supportNum;
+        this.replies = replies;
+    }
+
 }
--- UcountServer/src/main/java/cn/edu/nju/wonderland/ucountserver/vo/PostReplyVO.java
+++ UcountServer/src/main/java/cn/edu/nju/wonderland/ucountserver/vo/PostReplyVO.java
@@ -1,5 +1,22 @@
 package cn.edu.nju.wonderland.ucountserver.vo;
 
+/**
+ * 帖子回复信息VO
+ */
 public class PostReplyVO {
-    // TODO 帖子回复信息vo
+    public String username;
+    public String content;
+    public String time;
+    public int supportNum;
+
+    public PostReplyVO() {
+    }
+
+    public PostReplyVO(String username, String content, String time, int supportNum) {
+        this.username = username;
+        this.content = content;
+        this.time = time;
+        this.supportNum = supportNum;
+    }
+
 }
