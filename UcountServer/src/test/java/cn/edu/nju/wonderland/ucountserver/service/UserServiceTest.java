5c97123bcb65d4a2123c707faabd30eba8d724ad
TrouBleMaKerV <782024062@qq.com>
2017-09-06 08:09:52 +0800

    完善

--- UcountServer/src/test/java/cn/edu/nju/wonderland/ucountserver/service/UserServiceTest.java
+++ UcountServer/src/test/java/cn/edu/nju/wonderland/ucountserver/service/UserServiceTest.java
@@ -3,11 +3,16 @@ package cn.edu.nju.wonderland.ucountserver.service;
 import cn.edu.nju.wonderland.ucountserver.service.impl.UserServiceImpl;
 import cn.edu.nju.wonderland.ucountserver.vo.SignUpVO;
 import cn.edu.nju.wonderland.ucountserver.vo.UserInfoVO;
+import org.apache.catalina.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
@@ -17,23 +22,49 @@ public class UserServiceTest {
     UserService userService;
 
     @Test
-    public void testSignUp(){
+    public void testSignUp() {
         SignUpVO signUpVO = new SignUpVO();
-        signUpVO.userName = "123";
+        signUpVO.userName = "456";
         signUpVO.email = "123@qq.com";
-        signUpVO.password = "123";
+        signUpVO.password = MD5("123");
         signUpVO.tel = "123";
         String username = userService.signUp(signUpVO);
-        assertEquals(username,"123");
+        assertEquals(username, "456");
     }
+
     @Test
-    public  void testLogin(){
-        UserInfoVO userInfoVO = userService.login("123","123");
-        assertEquals(userInfoVO.userName,"123");
+    public void testLogin() {
+        UserInfoVO userInfoVO = userService.login("456", MD5("123"));
+        assertEquals(userInfoVO.userName, "456");
     }
+
     @Test
-    public void testGetUserInfo(){
-        UserInfoVO  userInfoVO = userService.getUserInfo("123");
-        assertEquals(userInfoVO.userName,"123");
+    public void testGetUserInfo() {
+        UserInfoVO userInfoVO = userService.getUserInfo("456");
+        assertEquals(userInfoVO.userName, "456");
+    }
+
+    @Test
+    public void testModefyUserInfo() {
+        UserInfoVO user = new UserInfoVO();
+        user.tel = "789";
+        user.userName = "456";
+        user.password = MD5("123");
+        user.email = "456@qq.com";
+        userService.modifyUserInfo(user.userName, user);
+        user = userService.getUserInfo("456");
+        assertEquals(user.email, "456@qq.com");
+        assertEquals(user.tel, "789");
+    }
+
+    private String MD5(String str) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            return new BigInteger(1, md.digest()).toString(16);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return null;
     }
 }
