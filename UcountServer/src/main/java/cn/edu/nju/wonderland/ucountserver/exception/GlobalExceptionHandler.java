633e17710046f0804a2642611aa8c4f76f0b2368
HuangXiao <hx36w35@163.com>
2017-09-08 20:46:57 +0800

    异常处理类修改

--- UcountServer/src/main/java/cn/edu/nju/wonderland/ucountserver/exception/GlobalExceptionHandler.java
+++ UcountServer/src/main/java/cn/edu/nju/wonderland/ucountserver/exception/GlobalExceptionHandler.java
@@ -6,37 +6,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static cn.edu.nju.wonderland.ucountserver.util.KeyName.ERROR;
-
 @ControllerAdvice
 @ResponseBody
 public class GlobalExceptionHandler {
 
-    private Map<String, String> getResponseMap(String message) {
-        Map<String, String> result = new HashMap<>();
-        result.put(ERROR, message);
-        return result;
-    }
-
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(ResourceNotFoundException.class)
-    public Map<String, String> resourceNotFoundHandler(ResourceNotFoundException e) {
-        return getResponseMap(e.getMessage());
+    public String resourceNotFoundHandler(ResourceNotFoundException e) {
+        return e.getMessage();
     }
 
     @ResponseStatus(value = HttpStatus.CONFLICT)
     @ExceptionHandler(ResourceConflictException.class)
-    public Map<String, String> resourceConflictHandler(ResourceConflictException e) {
-        return getResponseMap(e.getMessage());
+    public String resourceConflictHandler(ResourceConflictException e) {
+        return e.getMessage();
     }
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(InvalidRequestException.class)
-    public Map<String, String> invalidRequestHandler(InvalidRequestException e) {
-        return getResponseMap(e.getMessage());
+    public String invalidRequestHandler(InvalidRequestException e) {
+        return e.getMessage();
     }
 
 }
